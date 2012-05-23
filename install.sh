904c8ab18113181358c6dda0da61838ded1f7ada
Darko Fabijan <darko@renderedtext.com>
2012-05-23 16:22:33 +0200

    No autocorrect in zsh and source aliases.

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ sudo apt-get -y install zsh
 
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 
+echo "unsetopt correct_all" >> ~/.zshrc
+echo "source ~/.aliases" >> ~/.zshrc
+
 for f in gitconfig gemrc aliases rdebugrc
 do
   rm -f ~/.$f
