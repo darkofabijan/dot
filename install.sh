365e676e1feabe068cf83df94e8d4804d3654311
Darko Fabijan <darko@renderedtext.com>
2014-11-28 15:28:19 +0100

    Simplify install.

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-sudo apt-get -y install zsh
-
-wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-
-echo "unsetopt correct_all" >> ~/.zshrc
-echo "source ~/.aliases" >> ~/.zshrc
-echo "export EDITOR=vim" >> ~/.zshrc
-
 for f in gitconfig gemrc aliases rdebugrc
 do
   rm -f ~/.$f
